1dad9087c0e2ee45012e6f89775f4f754775032a
elMolda <david_molano@hotmail.com>
2018-09-06 10:36:41 -0500

    BackTracking hecho

--- lis.py
+++ lis.py
@@ -30,18 +30,40 @@ def lis_memo(arr,prev,curr,d):
 	d[prev+1][curr] = max(taken,nontaken)
 	return d[prev+1][curr]
 
-def lis_dp(arr): 
-    n = len(arr) 
- 
-    lis = [1]*n 
- 
-    for i in range (1 , n): 
-        for j in range(0 , i): 
-            if arr[i] > arr[j] and lis[i]< lis[j] + 1 : 
-                lis[i] = lis[j]+1
-  
-    return max(lis)
+def lis(arr):
+    n = len(arr)
 
-print(lis_aux([5,2,8,6,3,6,9,7]))
-print(lis_memo_aux([5,2,8,6,3,6,9,7]))
-print(lis_dp([5,2,8,6,3,6,9,7]))
+    lis = [1]*n
+
+    prev = [0]*n
+    for i in range(0, n):
+        prev[i] = i
+
+    for i in range (1 , n):
+        for j in range(0 , i):
+            if arr[i] > arr[j] and lis[i]< lis[j] + 1:
+                lis[i] = lis[j]+1
+                prev[i] = j
+    
+    maximum = 0
+    idx = 0
+
+    for i in range(n):
+        if maximum < lis[i]:
+            maximum = lis[i]
+            idx = i
+
+    seq = [arr[idx]]
+    while idx != prev[idx]:
+        idx = prev[idx]
+        seq.append(arr[idx])
+
+    return (maximum, reversed(seq))
+
+arr = [5,2,8,6,3,6,9,7]
+ans = lis(arr)
+print("Length of lis is", ans[0])
+print("The longest sequence is", ", ".join(str(x) for x in ans[1]))
+
+'''print(lis_aux([5,2,8,6,3,6,9,7]))
+print(lis_memo_aux([5,2,8,6,3,6,9,7]))'''
